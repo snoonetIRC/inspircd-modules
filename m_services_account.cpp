b6ae230b272064da2a0da705b43d2736ab961d89
nekosune <kat.swales@nekokittygames.com>
2016-08-30 19:34:17 +0100

    new Mode

--- m_services_account.cpp
+++ m_services_account.cpp
@@ -52,23 +52,40 @@ inline bool is_number(const std::string& s)
     return !s.empty() && it == s.end();
 }
 
-// User mode V - verified for X days
-class User_V : public ModeHandler
+class CommandSetAge : public Command
 {
-    
 public:
-    User_V(Module* Creator) : ModeHandler(Creator, "u_user_age", 'V', PARAM_ALWAYS, MODETYPE_USER) { }
-    
-    ModeAction OnModeChange(User* source, User* dest, Channel* channel, std::string &parameter, bool adding)
-    {
-        //if (!IS_LOCAL(source))
-        //{
-        if(adding)
-        {
-            dest->SetMode('V',true);
-            GetAccountAgeExtItem()->set(dest,parameter);
-            return MODEACTION_ALLOW;
-        }
+	CommandSetAge(Module *parent) : Command(parent, "SETAGE", 3, 3)
+	{
+		this->syntax = "<age>";
+	}
+
+	CmdResult Handle(const std::vector<std::string> &parameters, User *user)
+	{
+		LocalUser *localUser = IS_LOCAL(user);
+//		if(!localUser)
+//		{
+			GetAccountAgeExtItem()->set(user,parameters[0]);
+//		}
+	}
+};
+// User mode V - verified for X days
+//class User_V : public ModeHandler
+//{
+//    
+//public:
+//    User_V(Module* Creator) : ModeHandler(Creator, "u_user_age", 'V', PARAM_ALWAYS, MODETYPE_USER) { }
+//    
+//    ModeAction OnModeChange(User* source, User* dest, Channel* channel, std::string &parameter, bool adding)
+//    {
+//        //if (!IS_LOCAL(source))
+//        //{
+//        if(adding)
+//        {
+//            dest->SetMode('V',true);
+//            GetAccountAgeExtItem()->set(dest,parameter);
+//            return MODEACTION_ALLOW;
+//        }
         
         //}
         //else
@@ -76,21 +93,21 @@ public:
         //	source->WriteNumeric(500, "%s :Only a server may modify the +V user mode", source->nick.c_str());
         //}
         //return MODEACTION_DENY;
-    }
-    std::string GetUserParameter(User* useor)
-    {
-        return *(GetAccountAgeExtItem()->get(useor));
-    }
-    int GetNumParams(bool adding)
-    {
-        return 1;
-    }
-    void OnParameterMissing(User* user, User* dest, Channel* channel)
-    {
-        user->WriteServ("NOTICE %s :*** The user mode +V requires a parameter. Please provide a parameter, e.g. '+V *'.",
-                        user->nick.c_str());
-    }
-};
+//    }
+//    std::string GetUserParameter(User* useor)
+//    {
+//        return *(GetAccountAgeExtItem()->get(useor));
+//    }
+//    int GetNumParams(bool adding)
+//    {
+//        return 1;
+//    }
+//    void OnParameterMissing(User* user, User* dest, Channel* channel)
+//    {
+//        user->WriteServ("NOTICE %s :*** The user mode +V requires a parameter. Please provide a parameter, e.g. '+V *'.",
+//                        user->nick.c_str());
+//    }
+//};
 
 /** Channel mode +r - mark a channel as identified
  */
@@ -147,34 +164,40 @@ class User_r : public ModeHandler
 
 /** Channel mode +R - unidentified users cannot join
  */
-class AChannel_R : public ModeHandler
+//class AChannel_R : public ModeHandler
+//{
+//	AccountAgeBanExtItem &m_ext;
+// public:
+//	AChannel_R(Module* Creator,AccountAgeBanExtItem &e) : ModeHandler(Creator, "reginvite", 'R', PARAM_SETONLY, MODETYPE_CHANNEL), m_ext(e)
+// { }
+//	ModeAction OnModeChange(User* source, User* dest, Channel* channel, std::string &parameter, bool adding)
+//	{
+//
+//		if(adding)
+//		{
+//			if(!is_number(parameter))
+//				return MODEACTION_DENY;
+//			m_ext.set(channel,parameter);
+//			channel->SetModeParam(GetModeChar(),parameter);
+//			return MODEACTION_ALLOW;
+//		}
+//		else
+//		{
+//			if (!channel->IsModeSet(GetModeChar()))
+//				return MODEACTION_DENY;
+//
+//			m_ext.unset(channel);
+//			channel->SetModeParam(GetModeChar(), "");
+//			return MODEACTION_ALLOW;
+//		}
+//		
+//	}
+//};
+
+class AChannel_R : public SimpleChannelModeHandler
 {
-	AccountAgeBanExtItem &m_ext;
  public:
-	AChannel_R(Module* Creator,AccountAgeBanExtItem &e) : ModeHandler(Creator, "reginvite", 'R', PARAM_SETONLY, MODETYPE_CHANNEL), m_ext(e)
- { }
-	ModeAction OnModeChange(User* source, User* dest, Channel* channel, std::string &parameter, bool adding)
-	{
-
-		if(adding)
-		{
-			if(!is_number(parameter))
-				return MODEACTION_DENY;
-			m_ext.set(channel,parameter);
-			channel->SetModeParam(GetModeChar(),parameter);
-			return MODEACTION_ALLOW;
-		}
-		else
-		{
-			if (!channel->IsModeSet(GetModeChar()))
-				return MODEACTION_DENY;
-
-			m_ext.unset(channel);
-			channel->SetModeParam(GetModeChar(), "");
-			return MODEACTION_ALLOW;
-		}
-		
-	}
+	AChannel_R(Module* Creator) : SimpleChannelModeHandler(Creator, "reginvite", 'R') { }
 };
 
 /** User mode +R - unidentified users cannot message
@@ -200,7 +223,8 @@ class ModuleServicesAccount : public Module
 	AUser_R m3;
 	Channel_r m4;
 	User_r m5;
-	User_V m6;
+	//User_V m6;
+	CommandSetAge command;
 	AccountAgeExtItem accountage;
 	AccountAgeBanExtItem accountageban;
 	AccountAgeMuteExtItem accountagemute;
@@ -226,15 +250,15 @@ class ModuleServicesAccount : public Module
 	}
 
  public:
-	ModuleServicesAccount() : m1(this,accountageban), m2(this), m3(this), m4(this), m5(this),m6(this),
+	ModuleServicesAccount() : m1(this), m2(this), m3(this), m4(this), m5(this),//m6(this),
 		accountname("accountname", this),accountage("accountage",this), accountageban("accountageban",this),
-		accountagemute("accountagemute",this), checking_ban(false)
+		accountagemute("accountagemute",this), checking_ban(false),command(this)
 	{
 	}
 
 	void init()
 	{
-		ServiceProvider* providerlist[] = { &m1, &m2, &m3, &m4, &m5,&m6, &accountname, &accountage, &accountageban, &accountagemute };
+		ServiceProvider* providerlist[] = { &m1, &m2, &m3, &m4, &m5, &accountname, &accountage, &accountageban, &accountagemute,&command };
 		ServerInstance->Modules->AddServices(providerlist, sizeof(providerlist)/sizeof(ServiceProvider*));
 		Implementation eventlist[] = { I_OnWhois,I_OnWhoisLine, I_OnUserPreMessage, I_OnUserPreNotice, I_OnUserPreJoin, I_OnCheckBan,
 			I_OnDecodeMetaData, I_On005Numeric, I_OnUserPostNick, I_OnSetConnectClass };
@@ -413,12 +437,12 @@ class ModuleServicesAccount : public Module
 					user->WriteNumeric(477, user->nick + " " + chan->name + " :You need to be identified to a registered account to join this channel");
 					return MOD_RES_DENY;
 				}
-				if(acctage<age)
-				{
-					user->WriteNumeric(477, user->nick + " " + chan->name + " :Your account needs to be "+agestring->c_str() +" to enter this channel");
-					return MOD_RES_DENY;
-				}
-
+				//if(acctage<age)
+				//{
+				//	user->WriteNumeric(477, user->nick + " " + chan->name + " :Your account needs to be "+agestring->c_str() +" to enter this channel");
+				//	return MOD_RES_DENY;
+				//}
+//
 			}
 		}
 		return MOD_RES_PASSTHRU;
