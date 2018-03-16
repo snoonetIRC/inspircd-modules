656d0fe2a678ae0c73eb623a810890e8f8517495
linuxdaemon <linuxdaemon@snoonet.org>
2018-03-15 19:05:55 -0500

    Fix oper check on mode unset

--- 2.0/m_globalmessageflood.cpp
+++ 2.0/m_globalmessageflood.cpp
@@ -117,7 +117,7 @@ class GlobalMsgFlood : public ModeHandler
 			if (!channel->IsModeSet('x'))
 				return MODEACTION_DENY;
 
-			if (!source->IsModeSet('o'))
+			if (IS_LOCAL(source) && !source->IsModeSet('o'))
 			{
 				source->WriteNumeric(481, "%s %s :Permission Denied - Only operators may set channel mode x",source->nick.c_str(),channel->name.c_str());
 				return MODEACTION_DENY;
